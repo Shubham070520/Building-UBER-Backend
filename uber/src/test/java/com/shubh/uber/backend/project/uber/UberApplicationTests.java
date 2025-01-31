7dafd7b0fdc42d8d4b0263f848f4377ae6034a1d
Shubham Rane <shubhamrane016@gmail.com>
2025-01-31 21:05:57 +0530

    Email Service Testing

--- uber/src/test/java/com/shubh/uber/backend/project/uber/UberApplicationTests.java
+++ uber/src/test/java/com/shubh/uber/backend/project/uber/UberApplicationTests.java
@@ -1,13 +1,25 @@
 package com.shubh.uber.backend.project.uber;
 
+import com.shubh.uber.backend.project.uber.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+
+		emailSenderService.sendEmail(
+				"shubhamrane018@gmail.com",
+				"Hello,This is testing email service !",
+				"Body of my email"
+		);
+
 	}
 
 }
